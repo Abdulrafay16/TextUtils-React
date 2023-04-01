5bee1d86db90f2600897c0e3c190e64836e91409
Abdul Rafay <99479946+Abdulrafay16@users.noreply.github.com>
2023-04-01 17:11:48 +0500

    Add files via upload

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 
 
@@ -8,36 +8,70 @@ import React, {useState} from 'react'
 export default function TextForm(props) {
 
 
-  const handleOnchange = (event) =>{
-    console.log("on change")
+  const handleOnchange = (event) => {
+    // console.log("on change")
     setText(event.target.value)
-    
+
   }
-
-
-  const handleClick = () =>{
-    console.log("uppercase was clicked" + text)
+  const handleUpClick = () => {
+    // console.log("uppercase was clicked" + text)
     let newText = text.toUpperCase();
     setText(newText)
   }
-  
-  const handleClick2 = () =>{
-    console.log("LowerCase was clicked " + text)
-    let newText2 = text.toLowerCase();
-    setText(newText2)
+  const handleLoClick = () => {
+    // console.log("LowerCase was clicked " + text)
+    let newText = text.toLowerCase();
+    setText(newText)
   }
+  const handclearclick = () => {
+    let newText = ""
+    setText(newText)
+  }
+  const [text, setText] = useState("")
 
-const [text , setText] = useState("Enter Text Here")
+  const [myStyle, setmystyle] = useState({
+    color: 'black',
+    backgroundColor: 'white'
+  })
+  const [btnText, setBtnText] = useState("Dark Mode")
+
+  const handletoggleclick = () =>{
+    if(myStyle.color == 'black'){
+        setmystyle({
+            color: 'white',
+            backgroundColor: 'black'
+        })
+      setBtnText(" Light Mode")
+    }
+    else{
+        setmystyle({
+            color: 'black',
+            backgroundColor: 'white'
+        })
+        setBtnText("Dark Mode")
+    }
+}
 
   return (
     <>
+      <div className='container' style={myStyle}>
         <h2>{props.Heading}</h2>
-        <div className="mb-3">
-          <textarea className="form-control" value={text}  onChange={handleOnchange} rows={8} defaultValue={""} />
+        <div className="mb-3" >
+          <textarea  style={myStyle} className="form-control" value={text} onChange={handleOnchange} rows={8} defaultValue={""} />
         </div>
-        <button className='btn btn-primary' onClick={handleClick}>Convert to UpperCase</button>
-        <button className='btn btn-primary' onClick={handleClick2}>Convert to LowerCase</button>
+        <button className='btn btn-primary mx-3 my-3' onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className='btn btn-primary mx-3  my-3' onClick={handleLoClick}>Convert to LowerCase</button>
+        <button className='btn btn-primary mx-3  my-3' onClick={handclearclick} >Clear Text</button>
+        <button className='btn btn-primary mx-3  my-3' onClick={handletoggleclick}>{btnText}</button>
         {/* <button className='btn btn-primary' onClick={handleClickbold}>Convert to Bold </button> */}
+      </div>
+      <div className='container my-3' style={myStyle}>
+        <h2>Your Text Summary</h2>
+        <p>{text.split(" ").length} Words and {text.length} characters </p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
     </>
   )
 }
