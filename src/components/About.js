bf155071c7b0229a3c0c145c74a8037639627d5b
Abdul Rafay <99479946+Abdulrafay16@users.noreply.github.com>
2023-04-29 17:55:10 +0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,15 @@
 import React, {useState} from 'react'
+import Navbar from './Navbar'
+import Alert from './Alert'
 
-export default function About() {
 
+export default function About(props) {
+
+    // let myStyle = {
+    //     color: props.mode==="dark"?"white":"#113a62",
+    //     backgroundColor: props.mode==="dark"?"#113a62":"white"
+    // }
+    const [mode, setmode] = useState('light')
     const [myStyle, setmystyle] = useState({
         color: 'black',
         backgroundColor: 'white'
@@ -24,14 +32,41 @@ export default function About() {
             setBtnText("Dark Mode")
         }
     }
+    const [alert, setAlert] = useState(null)
+    const showAlert = (message, type) => {
+      setAlert({
+        msg: message,
+        type: type
+      })
+    }
+  
+    setTimeout(() => {
+      setAlert(null)
+    }, 3000);
+  
+    const togglemode = () => {
+        if (mode === 'light') {
+          setmode('dark')
+          document.body.style.backgroundColor = '#113a62'
+          showAlert("Dark mode has been enable", "success");
+        } else {
+          setmode('light')
+          document.body.style.backgroundColor = 'white'
+          showAlert("Light mode has been enable", "success");
+        } 
+      }
+    
     return (
-        <div className='container' style={myStyle}>
+        <>
+        <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} togglemode={togglemode}/>
+             <Alert alert={alert} />
+        <div className='container my-4' style={myStyle}>
             <h2 className='my-4'>About us</h2>
             <div className="accordion" id="accordionExample">
                 <div className="accordion-item">
                     <h2 className="accordion-header">
                         <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                            Accordion Item #1
+                     Analyze Your Text
                         </button>
                     </h2>
                     <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
@@ -43,7 +78,7 @@ export default function About() {
                 <div className="accordion-item">
                     <h2 className="accordion-header">
                         <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                            Accordion Item #2
+                 Free to use 
                         </button>
                     </h2>
                     <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
@@ -55,7 +90,7 @@ export default function About() {
                 <div className="accordion-item">
                     <h2 className="accordion-header">
                         <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            Accordion Item #3
+                    Browser compatible
                         </button>
                     </h2>
                     <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
@@ -65,13 +100,10 @@ export default function About() {
                     </div>
                 </div>
             </div>
-            <div className='container '>
+            {/* <div className='container '>
             <button onClick={toggleStyle} type="button" className="btn btn-primary my-3">{btnText}</button>
-            </div>
+            </div> */}
         </div>
-        
-    )
-}
-
+        </>
     )
 }
