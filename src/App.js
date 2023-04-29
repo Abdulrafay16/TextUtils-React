f042846b8968a4dfd2c08ee4e2e04d5b07c6216c
Abdul Rafay <99479946+Abdulrafay16@users.noreply.github.com>
2023-04-29 17:54:04 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 import Alert from './components/Alert';
 // import Translate from './components/Translate';
-// import About from './components/About';
+import {BrowserRouter, Route,Routes} from "react-router-dom";
+
 
 
 
@@ -14,8 +16,6 @@ import Alert from './components/Alert';
 function App() {
   const [mode, setmode] = useState('light')
   const [alert, setAlert] = useState(null)
-
-
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
@@ -45,12 +45,25 @@ function App() {
     <>
       {/* <About/> */}
       {/* <Translate /> */}
-
+{/* 
       <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} togglemode={togglemode} />
       <Alert alert={alert} />
       <div className='container my-3'>
-        <TextForm showAlert={showAlert} Heading="Enter the text to analyze below"  />
-      </div>
+        <TextForm showAlert={showAlert} Heading="Enter the text to analyze below" />
+        <About />
+      </div> */}
+
+
+      <Alert alert={alert} />
+      <BrowserRouter>
+      <Routes>
+        <Route path='/navbar' element={<Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} togglemode={togglemode}/>}/>
+        <Route path='/textform' element={<TextForm showAlert={showAlert} Heading="Enter the text to analyze below" />}/>
+        <Route path='/about' element={  <About mode={mode} />} />
+        
+      </Routes>
+      </BrowserRouter>
+   
     </>
   );
 }
